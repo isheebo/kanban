49d2d5b993e9c125805c77af06c3f0ab58c4705b
isheebo <kedgar751@gmail.com>
2017-08-31 19:43:57 +0300

    Create a docopt decorator function and add it to main

---
+++ main.py
@@ -0,0 +1,54 @@
+"""
+Usage:
+    kanban todo <task_name>
+    kanban doing <task_id>
+    kanban done <task_id>
+    kanban edit_task <task_id>
+    kanban delete_task <task_id>
+    kanban list_todo
+    kanban list_doing
+    kanban list_done
+    kanban list_all
+
+    kanban quit
+Options:
+  -h --help     Show this screen.
+"""
+
+import cmd
+import os
+from docopt import DocoptExit, docopt
+from todo.todo import ToDo
+
+
+def docopt_cmd(func):
+    """
+    This decorator simplifies the try/except block and returns
+    the result of parsing docopt using an action
+
+    credits: https://github.com/docopt/docopt/blob/master/examples/interactive_example.py
+    Contributors: JonLundy, TheWaWaR
+    """
+
+    def fn(self, arg):
+        try:
+            opt = docopt(fn.__doc__, arg)
+
+        except DocoptExit as err:
+            # The DocoptExit is thrown when the args do not match
+            # We print a message to the user and the usage block
+            print('Invalid Command!')
+            print(err)
+            return
+
+        except SystemExit:
+            # The SystemExit exception prints the usage for --help
+            # We do not need to do the print here
+            return
+
+        return func(self, opt)
+
+    fn.__name__ = func.__name__
+    fn.__doc__ = func.__doc__
+    fn.__dict__.update(func.__dict__)
+    return fn

f0753ada15f0bc6bc113c4425278c18bf2c450ba
isheebo <kedgar751@gmail.com>
2017-08-31 19:35:54 +0300

    Add the done method

--- app/kanban.py
+++ app/kanban.py
@@ -33,3 +33,64 @@ class ToDo:
                             (task_name, task_description, "ToDo"))
         self.db.commit()
         return f"A task with name '{task_name.title()}' has been created successfully!"
+
+    def doing(self, task_id):
+        """ We can only  mark a status as 'doing' only and only if it was on the todo list. """
+        self.cursor.execute(f"SELECT Status FROM Task WHERE ID = {task_id}")
+        results = self.cursor.fetchone()
+        if results:
+            status = results[0]
+            if status == "ToDo":
+                try:
+                    self.cursor.execute("UPDATE Task SET Status ='Doing',Start='{0}' WHERE ID = {1}".format(
+                        datetime.now().strftime("%Y-%m-%d %H:%M:%S"), task_id))
+                    self.db.commit()
+                    return f"Task with ID {task_id} has been successfully updated!"
+                except sqlite3.OperationalError:
+                    print("Your task ID doesn't exist!")
+
+            elif status == "Doing":
+                return "You are already doing this task"
+
+            elif status == "Done":
+                # if task exists and has been marked as done, we can change it's
+                # status to doing directly to reduce on data duplication within
+                # the database
+                try:
+                    self.cursor.execute(
+                        "UPDATE Task SET Status ='Doing',Start='{0}', Finish='NULL' WHERE ID = {1}".format(
+                            datetime.now().strftime("%Y-%m-%d %H:%M:%S"), task_id))
+                    self.db.commit()
+                    return f"Task with ID {task_id} has been successfully updated!"
+                except sqlite3.OperationalError as err:
+                    return f"{err}"
+            return "Unknown task status: status can only be done, todo or doing"
+        return "Task with the specified ID doesn't exist.. please try listing the tasks and try again"
+
+    def done(self, task_id):
+        """ We can only check a task as done.. only and only if we have been doing it.
+        i.e: It's past status must be 'doing' """
+        self.cursor.execute(f"SELECT Status FROM Task WHERE ID = {task_id}")
+        results = self.cursor.fetchone()
+        if results:
+            status = results[0]
+            if status == "Doing":
+                try:
+                    self.cursor.execute(
+                        f"SELECT Start FROM Task WHERE ID={task_id}")
+                    start_time = datetime.strptime(
+                        self.cursor.fetchone()[0], "%Y-%m-%d %H:%M:%S")
+                    finish_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                    duration = datetime.strptime(
+                        finish_time, "%Y-%m-%d %H:%M:%S") - start_time
+                    self.cursor.execute(
+                        "UPDATE Task SET Status='Done', Finish='{0}', Duration='{1}'  WHERE ID = {2}".format(
+                            finish_time, duration, task_id))
+                    self.db.commit()
+                    return f"Task with ID {task_id} has successfully been completed!"
+                except sqlite3.OperationalError:
+                    return "unknown task ID"
+            if status == "Done":
+                return "Task has already been completed"
+            return "The task specified is still on the todo list: try changing it's status first"
+        return "unknown task ID: try listing the tasks first so as to select the appropriate ID"
